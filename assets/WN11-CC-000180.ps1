55f6f3194db8e58be1e05285f273cb320e355495
Ken T. <kent28808@gmail.com>
2026-08-18 12:36:47 -0700

    Create WN11-CC-000180.ps1

---
+++ assets/WN11-CC-000180.ps1
@@ -0,0 +1,27 @@
+<#
+.SYNOPSIS
+    This PowerShell script remediates WN11-CC-000180 by disabling Autoplay for non-volume devices.
+
+.NOTES
+    Author          : Ken T.
+    LinkedIn        : linkedin.com/in/kent28808/
+    GitHub          : github.com/kent28808
+    Date Created    : 2026-18-08
+    Last Modified   : 2026-18-08
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-CC-000180
+    Documentation   : https://stigaview.com/products/win11/v2r7/WN11-CC-000180/
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\WN11-AU-000180.ps1 
+#>
