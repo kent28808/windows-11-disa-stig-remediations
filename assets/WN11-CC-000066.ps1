161b62cb44a434db6ca8073f97d982adaa6cb13a
Ken T. <kent28808@gmail.com>
2026-08-18 11:55:42 -0700

    Create WN11-CC-000066.ps1

---
+++ assets/WN11-CC-000066.ps1
@@ -0,0 +1,27 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that the maximum size of the Windows Application event log is at least 32768 KB (32 MB).
+
+.NOTES
+    Author          : Ken T.
+    LinkedIn        : linkedin.com/in/kent28808/
+    GitHub          : github.com/kent28808
+    Date Created    : 2026-18-08
+    Last Modified   : 2026-18-08
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-CC-000066
+    Documentation   : https://stigaview.com/products/win11/v2r7/WN11-CC-000066/
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\WN11-CC-000066.ps1 
+#>
