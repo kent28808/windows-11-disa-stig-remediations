99d887ee0b4dd4cc9f1c8eb615f7db71bfa8afeb
Ken T. <kent28808@gmail.com>
2026-08-18 12:23:47 -0700

    Create WN11-CC-000326.ps1

---
+++ assets/WN11-CC-000326.ps1
@@ -0,0 +1,28 @@
+<#
+.SYNOPSIS
+    This PowerShell script remediates WN11-CC-000326 by enabling PowerShell Script Block Logging.
+
+
+.NOTES
+    Author          : Ken T.
+    LinkedIn        : linkedin.com/in/kent28808/
+    GitHub          : github.com/kent28808
+    Date Created    : 2026-18-08
+    Last Modified   : 2026-18-08
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-CC-000326
+    Documentation   : https://stigaview.com/products/win11/v2r7/WN11-CC-000326/
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\WN11-CC-000326.ps1 
+#>
