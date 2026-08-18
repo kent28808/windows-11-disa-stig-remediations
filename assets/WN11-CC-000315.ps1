5487915ec88c2f2d404f3af284049b850d940bfa
Ken T. <kent28808@gmail.com>
2026-08-18 12:34:41 -0700

    Create WN11-CC-000315.ps1

---
+++ assets/WN11-CC-000315.ps1
@@ -0,0 +1,27 @@
+<#
+.SYNOPSIS
+    This PowerShell script remediates WN11-CC-000315 by disabling elevated privileges for Windows Installer packages.
+
+.NOTES
+    Author          : Ken T.
+    LinkedIn        : linkedin.com/in/kent28808/
+    GitHub          : github.com/kent28808
+    Date Created    : 2026-18-08
+    Last Modified   : 2026-18-08
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-CC-000315
+    Documentation   : https://stigaview.com/products/win11/v2r7/WN11-CC-000315/
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\WN11-CC-000315.ps1 
+#>
