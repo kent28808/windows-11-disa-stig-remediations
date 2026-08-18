5a5ee309468a86f2ecd7764660c7041e08a12137
Ken T. <kent28808@gmail.com>
2026-08-18 12:24:24 -0700

    Update WN11-AU-000050.ps1

--- assets/WN11-AU-000050.ps1
+++ assets/WN11-AU-000050.ps1
@@ -1,6 +1,6 @@
 <#
 .SYNOPSIS
-    This PowerShell script ensures that the maximum size of the Windows Application event log is at least 32768 KB (32 MB).
+    This PowerShell script remediates WN11-AU-000050 by enabling success auditing for process creation events.
 
 .NOTES
     Author          : Ken T.
